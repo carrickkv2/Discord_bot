7abe556c1fc70fdf24ceb630660fdf094c7f8420
Dave <davidasmah@hotmail.com>
2021-04-21 14:42:57 -0400

    Updated files.

--- cogs/utils/paths_for_functions.py
+++ cogs/utils/paths_for_functions.py
@@ -1,5 +1,5 @@
-# path_for_csv_to_dict = r'G:\Python-Projects\Pip\Discord Test\Discord Bot\csv'
+path_for_csv_to_dict = r'G:\Python-Projects\Pip\Discord Test\Discord Bot\csv'
 
-# path_for_csv_output_for_both_rename_and_extract = r'C:\output_csv'
+path_for_csv_output_for_both_rename_and_extract = r'C:\output_csv'
 
-# path_for_write_csv = r'C:\output_csv\table.csv'
+path_for_write_csv = r'C:\output_csv\table.csv'
