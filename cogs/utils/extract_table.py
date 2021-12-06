29f6134203aa470529682ed52fa689323c616cd6
carrickkv2 <davidasmah@hotmail.com>
2021-12-06 13:12:35 +0000

    Removed redundant comments.

--- cogs/utils/extract_table.py
+++ cogs/utils/extract_table.py
@@ -12,11 +12,8 @@ def extract_to_csv(image_url: str, output_folder: typing.Union[str, bytes, os.Pa
     load_dotenv()
     api = getenv("API")
     et_sess = ExtractTable(api_key=api)  # Replace your VALID API Key here
-    # print(et_sess.check_usage())
 
     image_location = image_url
-    # output_extract = str(et_sess.ServerResponse.json())
-    # print(et_sess.ServerResponse.json())
 
     # Checks the API Key validity as well as shows associated plan usage
     et_sess.process_file(filepath=image_location, output_format="csv")
